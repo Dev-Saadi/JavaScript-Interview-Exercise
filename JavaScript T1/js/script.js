d9c374db362611fb16a61d2fd320744d8b5bdbe3
Dev-Saadi <ahmedshaikhsaadi@gmail.com>
2025-01-26 22:05:17 +0600

    learning to fly

--- JavaScript T1/js/script.js
+++ JavaScript T1/js/script.js
@@ -1,10 +1,35 @@
 const firstNumberInput = document.getElementById("firstNumber");
 const secondNumberInput = document.getElementById("secondNumber");
 const operatorsInput = document.getElementById("operators");
-let resultInput = document.getElementById("result");
+const calculatebtn = document.getElementById("calculate");
+const resultDisplay = document.getElementById("result");
+const reset = document.getElementById("resetbtn");
 
-const firstNumber = firstNumberInput.value;
-const secondNumber = secondNumberInput.value;
-const operators = operatorsInput.value;
+function calculate() {
+  const firstNumber = parseFloat(firstNumberInput.value);
+  const secondNumber = parseFloat(secondNumberInput.value);
+  const operators = operatorsInput.value;
 
-console.log(firstNumber);
+  if (operators === "+") {
+    result = firstNumber + secondNumber;
+  } else if (operators === "-") {
+    result = firstNumber - secondNumber;
+  } else if (operators === "*") {
+    result = firstNumber * secondNumber;
+  } else if (operators === "/") {
+    result = firstNumber / secondNumber;
+  }
+
+  resultDisplay.textContent = "Result: " + result;
+}
+calculatebtn.addEventListener("click", calculate);
+
+function refresh() {
+  firstNumberInput.value = "";
+  secondNumberInput.value = "";
+  operatorsInput.value = "+";
+
+  resultDisplay.textContent = "Result: ";
+}
+
+reset.addEventListener("click", refresh);
