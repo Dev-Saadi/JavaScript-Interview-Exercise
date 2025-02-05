4220a628b45874a23d0cb99327a0ca22812978ac
Dev-Saadi <ahmedshaikhsaadi@gmail.com>
2025-02-05 15:43:37 +0600

    trial and error

--- Trial and error/js/script.js
+++ Trial and error/js/script.js
@@ -1,9 +1,16 @@
-const arr = [2, 3, 4, 5, 5];
+// const arr = [2, 3, 4, 5, 5];
 
-let sum = 0;
+// let sum = 0;
 
-for (i = 0; i < arr.length; i++) {
-  sum += arr[i];
+// for (i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+
+// console.log(sum);
+
+// Print number from 1 to 5 using a loop
+sum = 10;
+
+for (i = 5; i <= sum; i++) {
+  console.log(i);
 }
-
-console.log(sum);
